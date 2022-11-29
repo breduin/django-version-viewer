e94dcf54750cd3a8ba1e7dc05aa3813cb27cc979
Oleg Tarasov <tolegu@ya.ru>
2022-11-29 10:44:54 +0300

    change url to re_path

--- django_version_viewer/urls.py
+++ django_version_viewer/urls.py
@@ -1,14 +1,18 @@
-try:
-    # django 1.6+
-    from django.conf.urls import url
-except ImportError:
-    # django <1.6
-    from django.conf.urls.defaults import url
-
 from . import views
 
-urlpatterns = [
-    url(r'^$', views.DjangoVersionViewer.as_view(), name='django_version_viewer'),
-    url(r'^csv/$', views.DjangoVersionViewerCSV.as_view(), name='django_version_viewer_csv'),
-    url(r'^toolbar/$', views.DjangoVersionViewerToolBar.as_view(), name='django_version_viewer_toolbar'),  # noqa
-]
+try:
+    # django 3.2+
+    from django.urls import re_path
+    urlpatterns = [
+        re_path(r'^$', views.DjangoVersionViewer.as_view(), name='django_version_viewer'),
+        re_path(r'^csv/$', views.DjangoVersionViewerCSV.as_view(), name='django_version_viewer_csv'),
+        re_path(r'^toolbar/$', views.DjangoVersionViewerToolBar.as_view(), name='django_version_viewer_toolbar'),  # noqa
+    ]
+except ImportError:
+    # django <3.2
+    from django.conf.urls import url
+    urlpatterns = [
+        url(r'^$', views.DjangoVersionViewer.as_view(), name='django_version_viewer'),
+        url(r'^csv/$', views.DjangoVersionViewerCSV.as_view(), name='django_version_viewer_csv'),
+        url(r'^toolbar/$', views.DjangoVersionViewerToolBar.as_view(), name='django_version_viewer_toolbar'),  # noqa
+    ]
